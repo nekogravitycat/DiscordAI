6c1e96f12749b2220ba6497e962785823edaa015
Gravity cat <29692485+nekogravitycat@users.noreply.github.com>
2024-02-10 03:57:56 +0800

    Update jsondata.go

--- internal/jsondata/jsondata.go
+++ internal/jsondata/jsondata.go
@@ -10,7 +10,7 @@ import (
 
 func Check(file string, defaultData any) {
 	if _, err := os.Stat(file); errors.Is(err, os.ErrNotExist) {
-		fmt.Printf("'%s' does not exist, creating one with default values.\n", file)
+		fmt.Printf("'%s' does not exist, creating one with default values\n", file)
 		Save(file, defaultData)
 	}
 }
@@ -18,37 +18,37 @@ func Check(file string, defaultData any) {
 func Load(file string, dataPointer any) {
 	jsonFile, err := os.Open(file)
 	if err != nil {
-		fmt.Println("Error reading user.json")
+		fmt.Printf("Error reading '%s'\n", file)
 	}
 	defer jsonFile.Close()
 
 	byteValue, err := io.ReadAll(jsonFile)
 	if err != nil {
-		fmt.Println("Error reading bytes of user.json")
+		fmt.Printf("Error reading bytes of '%s'\n", file)
 	}
 
 	err = json.Unmarshal(byteValue, dataPointer)
 	if err != nil {
-		fmt.Println("Error parsing user.json into Users struct.")
+		fmt.Printf("Error parsing '%s'\n", file)
 	}
 }
 
 func Save(file string, data any) {
 	jsonFile, err := os.Create(file)
 	if err != nil {
-		fmt.Println("Error writing user.json")
+		fmt.Printf("Error writing '%s'\n", file)
 		return
 	}
 	defer jsonFile.Close()
 
 	jsonData, err := json.MarshalIndent(data, "", "  ")
 	if err != nil {
-		fmt.Println("Error parsing Users struct into json data.")
+		fmt.Println("Error parsing data into json.")
 		return
 	}
 
 	_, err = jsonFile.Write(jsonData)
 	if err != nil {
-		fmt.Println("Error writing user.json file.")
+		fmt.Printf("Error writing '%s'\n", file)
 	}
 }
