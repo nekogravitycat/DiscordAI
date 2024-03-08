748af90d788cf0971a0850bec1cc3840f3b81bb8
Gravity cat <29692485+nekogravitycat@users.noreply.github.com>
2024-03-08 22:20:50 +0800

    Update pricing.go

--- internal/pricing/pricing.go
+++ internal/pricing/pricing.go
@@ -27,6 +27,7 @@ type dallE2Pricing struct {
 }
 
 type pricingTable struct {
+	Rate              float32       `json:"rate"`
 	Gpt4TurboPreview  gptRate       `json:"gpt-4-turbo-preview"`
 	Gpt4VisionPreview gptRate       `json:"gpt-4-vision-preview"`
 	Gpt3Dot5Turbo     gptRate       `json:"gpt-3.5-turbo"`
@@ -36,6 +37,7 @@ type pricingTable struct {
 
 func newPricingTable() pricingTable {
 	pt := pricingTable{
+		Rate: 1.2,
 		Gpt4TurboPreview: gptRate{
 			Input:  0.01,
 			Output: 0.03,
@@ -72,51 +74,53 @@ func newPricingTable() pricingTable {
 var pricingData pricingTable = newPricingTable()
 
 func GetGPTCost(model string, usage openai.Usage) float32 {
+	var cost float32
 	switch model {
 	case openai.GPT4TurboPreview:
-		return float32(usage.PromptTokens)/1000*pricingData.Gpt4TurboPreview.Input +
+		cost = float32(usage.PromptTokens)/1000*pricingData.Gpt4TurboPreview.Input +
 			float32(usage.CompletionTokens)/1000*pricingData.Gpt4TurboPreview.Output
 	case openai.GPT4VisionPreview:
-		return float32(usage.PromptTokens)/1000*pricingData.Gpt4VisionPreview.Input +
+		cost = float32(usage.PromptTokens)/1000*pricingData.Gpt4VisionPreview.Input +
 			float32(usage.CompletionTokens)/1000*pricingData.Gpt4VisionPreview.Output
 	case openai.GPT3Dot5Turbo:
-		return float32(usage.PromptTokens)/1000*pricingData.Gpt3Dot5Turbo.Input +
+		cost = float32(usage.PromptTokens)/1000*pricingData.Gpt3Dot5Turbo.Input +
 			float32(usage.CompletionTokens)/1000*pricingData.Gpt3Dot5Turbo.Output
 	default:
-		return float32(usage.PromptTokens)/1000*pricingData.Gpt4TurboPreview.Input +
+		cost = float32(usage.PromptTokens)/1000*pricingData.Gpt4TurboPreview.Input +
 			float32(usage.CompletionTokens)/1000*pricingData.Gpt4TurboPreview.Output
 	}
+	return cost * pricingData.Rate
 }
 
 func GetDalleCost(model string, size string, quality string) float32 {
 	if model == openai.CreateImageModelDallE3 {
 		if quality == "standard" {
 			if size == "1024x1024" {
-				return pricingData.DallE3.Standard.R1024X1024
+				return pricingData.DallE3.Standard.R1024X1024 * pricingData.Rate
 			} else if size == "1024x1792" {
-				return pricingData.DallE3.Standard.R1024X1792
+				return pricingData.DallE3.Standard.R1024X1792 * pricingData.Rate
 			} else if size == "1792x1024" {
-				return pricingData.DallE3.Standard.R1792X1024
+				return pricingData.DallE3.Standard.R1792X1024 * pricingData.Rate
 			}
 		} else if quality == "hd" {
 			if size == "1024x1024" {
-				return pricingData.DallE3.Hd.R1024X1024
+				return pricingData.DallE3.Hd.R1024X1024 * pricingData.Rate
 			} else if size == "1024x1792" {
-				return pricingData.DallE3.Hd.R1024X1792
+				return pricingData.DallE3.Hd.R1024X1792 * pricingData.Rate
 			} else if size == "1792x1024" {
-				return pricingData.DallE3.Hd.R1792X1024
+				return pricingData.DallE3.Hd.R1792X1024 * pricingData.Rate
 			}
 		}
 	} else if model == openai.CreateImageModelDallE2 {
 		if size == "1024x1024" {
-			return pricingData.DallE2.R1024X1024
+			return pricingData.DallE2.R1024X1024 * pricingData.Rate
 		} else if size == "512x512" {
-			return pricingData.DallE2.R512X512
+			return pricingData.DallE2.R512X512 * pricingData.Rate
 		} else if size == "256x256" {
-			return pricingData.DallE2.R256X256
+			return pricingData.DallE2.R256X256 * pricingData.Rate
 		}
 	}
-	return pricingData.DallE3.Hd.R1024X1792
+	return pricingData.DallE3.Hd.R1024X1792 * pricingData.Rate
 }
 
 const PRICINGFILE = "./configs/pricing.json"
